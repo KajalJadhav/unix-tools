c648dff704174d2219d610811724af84d7536269
Kajal <kajal@inankhome.com>
2014-01-09 22:32:33 +0530

    Uniiq Done

--- src/kajaljad/unixtools/Uniq.java
+++ src/kajaljad/unixtools/Uniq.java
@@ -1,10 +1,14 @@
 package kajaljad.unixtools;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 class UniqOperations {
     public void uniq(String text) {
-        String[] result = text.spilt("\r\n");
-        for (int i = 1; i < result.length-1; i++) {
-            if(0 != result[i].compareTo(result[i+1])){
+        String[] result;
+        result = text.split("\r\n");
+        for (int i = 1; i < result.length - 1; i++) {
+            if (0 != result[i].compareTo(result[i + 1])) {
                 System.out.println(result[i]);
             }
         }
@@ -14,6 +18,17 @@ class UniqOperations {
 public class Uniq {
     public static void main(String[] args) {
         UniqOperations operations = new UniqOperations();
-
+        BufferedReader bufferedReader;
+        String fileContent = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(args[0]));
+            String currentLine;
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                fileContent += "\r\n" + currentLine;
+            }
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+        operations.uniq(fileContent);
     }
 }
