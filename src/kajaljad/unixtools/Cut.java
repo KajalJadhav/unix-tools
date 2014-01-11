5ba5d7f6bc7028045c1af5ba2ccab6784a36077e
Kajal <kajal@inankhome.com>
2014-01-11 10:06:32 +0530

    Cut working for Delimeter

--- src/kajaljad/unixtools/Cut.java
+++ src/kajaljad/unixtools/Cut.java
@@ -4,16 +4,16 @@ public class Cut {
     public static void main(String[] args) {
         CutOperations operations = new CutOperations();
         String fileContent;
-        if(args.length == 3) {
-            String separator = args[1].substring(2,3);
-            int fieldNumber = Integer.parseInt(args[0].substring(2,3));
+        if (args.length == 3) {
+            String separator = args[1].substring(2, 3);
+            int fieldNumber = Integer.parseInt(args[0].substring(2, 3));
             fileContent = operations.readFile(args[2]);
             operations.cutByDelimeter(fieldNumber, separator, fileContent);
         }
         try {
-            if(args.length == 2) {
+            if (args.length == 2) {
                 fileContent = operations.readFile(args[1]);
-                int fieldNumber = Integer.parseInt(args[0].substring(2,3));
+                int fieldNumber = Integer.parseInt(args[0].substring(2, 3));
                 operations.cutByField(fieldNumber, fileContent);
             }
         } catch (Exception e) {
--- src/kajaljad/unixtools/CutOperations.java
+++ src/kajaljad/unixtools/CutOperations.java
@@ -1,19 +1,20 @@
 package kajaljad.unixtools;
 
-public class CutOperations extends MyFileReader{
+public class CutOperations extends MyFileReader {
     public void cutByField(int fieldNumber, String input) {
         String[] result = input.split("\r\n");
         for (int i = 0; i < result.length; i++) {
             String[] content = result[i].split(" ");
-            if(content.length < fieldNumber)
+            if (content.length < fieldNumber)
                 System.out.println("\n");
             else
-                System.out.println(content[fieldNumber-1]);
+                System.out.println(content[fieldNumber - 1]);
         }
     }
-    public void cutByDelimeter(int fieldNumber,String separator,String input) {
+
+    public void cutByDelimeter(int fieldNumber, String separator, String input) {
         String[] result = input.split("\r\n");
-        for (int i = 0; i < result.length-1; i++) {
+        for (int i = 0; i < result.length - 1; i++) {
             System.out.println(result[i].split(separator)[fieldNumber]);
         }
     }
