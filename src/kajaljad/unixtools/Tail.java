dd06483867833b8ee679565c349df0c5881aa293
Kajal <kajal@inankhome.com>
2014-01-11 11:03:52 +0530

    Changes done in Tail

--- src/kajaljad/unixtools/Tail.java
+++ src/kajaljad/unixtools/Tail.java
@@ -1,9 +1,6 @@
 package kajaljad.unixtools;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-
-class TailOperations {
+class TailOperations extends MyFileReader{
     public void tail(int size, String text) {
         String[] result = text.split("\r\n");
         for (int i = result.length - size; i < result.length; i++) {
@@ -16,18 +13,14 @@ public class Tail {
     public static void main(String[] args) {
         TailOperations operations = new TailOperations();
         int length = 10;
-        BufferedReader bufferedReader;
         String fileContent = null;
         try {
             if (args.length == 2) {
                 length = Integer.parseInt(args[0]) * (-1);
-                bufferedReader = new BufferedReader(new FileReader(args[1]));
+                fileContent = operations.readFile(args[1]);
             } else {
-                bufferedReader = new BufferedReader(new FileReader(args[0]));
+                fileContent = operations.readFile(args[0]);
             }
-            String currentLine;
-            while ((currentLine = bufferedReader.readLine()) != null)
-                fileContent += "\r\n" + currentLine;
         } catch (Exception e) {
             System.out.println("Error");
         }
