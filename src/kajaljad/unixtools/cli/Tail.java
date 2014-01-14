29306b2646b6c471c165af01a7d83b7387267fcc
Kajal <kajal@inankhome.com>
2014-01-14 23:21:47 +0530

    Test Cases for Tail Done

--- src/kajaljad/unixtools/cli/Tail.java
+++ src/kajaljad/unixtools/cli/Tail.java
@@ -5,20 +5,23 @@ import kajaljad.unixtools.libraries.TailOperations;
 
 public class Tail {
     public static void main(String[] args) {
-        TailOperations operations = new TailOperations();
-        MyFileReader fileData = new MyFileReader();
+        MyFileReader rf = new MyFileReader();
         int length = 10;
-        String fileContent = null;
-        try {
-            if (args.length == 2) {
-                length = Math.abs(Integer.parseInt(args[0]));
-                fileContent = fileData.readFile(args[1]);
-            } else {
-                fileContent = fileData.readFile(args[0]);
-            }
-        } catch (Exception e) {
-            System.err.println("Error");
+        String fileData;
+        String fileName = args[0];
+        if (args.length == 2) {
+            length = Math.abs(Integer.parseInt(fileName));
+            fileName = args[1];
         }
-        operations.tail(length, fileContent);
+        fileData = rf.readFile(fileName);
+        TailOperations tailOperations = new TailOperations(length, fileData);
+        String lines = tailOperations.tail();
+        String[] result = lines.split("\r\n");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String line : result) {
+            stringBuilder.append(line);
+            stringBuilder.append("\n");
+        }
+        System.out.println(stringBuilder.toString());
     }
 }
--- src/kajaljad/unixtools/libraries/TailOperations.java
+++ src/kajaljad/unixtools/libraries/TailOperations.java
@@ -1,10 +1,20 @@
 package kajaljad.unixtools.libraries;
 
-public class TailOperations{
-    public void tail(int size, String text) {
-        String[] result = text.split("\r\n");
-        for (int i = result.length - size; i < result.length; i++) {
-            System.out.println(result[i]);
-        }
+public class TailOperations {
+    private String text;
+    private int limit;
+
+    public TailOperations(int noOfLines, String text) {
+        this.text = text;
+        this.limit = noOfLines;
+    }
+
+    public String tail() {
+        String[] lines = text.split("\r\n");
+        if (lines.length < limit) return lines.toString();
+        StringBuilder result = new StringBuilder();
+        for (int i = (lines.length - limit); i < lines.length; i++)
+            result.append(lines[i]).append("\r\n");
+        return result.toString();
     }
 }
