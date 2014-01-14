71e797acb4f580a8608f734858db3625f77f9801
Kajal <kajal@inankhome.com>
2014-01-15 00:38:25 +0530

    Changes Done in Cut And Cut By Character Functionality Done

--- src/kajaljad/unixtools/cli/Cut.java
+++ src/kajaljad/unixtools/cli/Cut.java
@@ -1,24 +1,37 @@
 package kajaljad.unixtools.cli;
 
+import kajaljad.unixtools.filesystem.MyFileReader;
 import kajaljad.unixtools.libraries.CutOperations;
 
 public class Cut {
     public static void main(String[] args) {
         CutOperations operations = new CutOperations();
+        MyFileReader readContent = new MyFileReader();
         String fileContent;
+        if (args.length == 3) {           // for delimiter
+            String separator = args[1].substring(2, 3);
+            int fieldNumber = Integer.parseInt(args[0].substring(2, 3));
+            fileContent = readContent.readFile(args[2]);
+            String data = operations.cutByFieldAndSeparator(fieldNumber, separator, fileContent);
+            String[] result = data.split("\r\n");
+            for (Object o : result)
+                System.out.println(o);
+        }
         try {
-            if (args.length == 3) {
-                String separator = args[1].substring(2, 3);
+            if (args.length == 2) {           // for field and character
+                fileContent = readContent.readFile(args[1]);
                 int fieldNumber = Integer.parseInt(args[0].substring(2, 3));
-                fileContent = operations.readFile(args[2]);
-                operations.cutByDelimeter(fieldNumber, separator, fileContent);
-            }
-            if (args.length == 2) {
-                fileContent = operations.readFile(args[1]);
-                operations.cutByField(fileContent);
+                String character = args[0].substring(0, 2);
+                String data;
+                if (0 == character.compareTo("-f"))
+                    data = operations.cutByField(fileContent);
+                else data = operations.cutByCharacter(fieldNumber, fileContent);
+                String[] result = data.split("\r\n");
+                for (Object o : result)
+                    System.out.println(o);
             }
         } catch (Exception e) {
-            System.out.println("Error");
+            System.err.println("Error");
         }
     }
 }
--- src/kajaljad/unixtools/libraries/CutOperations.java
+++ src/kajaljad/unixtools/libraries/CutOperations.java
@@ -1,23 +1,32 @@
 package kajaljad.unixtools.libraries;
 
-import kajaljad.unixtools.filesystem.MyFileReader;
-
-public class CutOperations extends MyFileReader {
-    public void cutByField(String input) {
-        String[] result = input.split("\r\n");
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);
-        }
+public class CutOperations {
+    public String cutByCharacter(int number, String text) {
+        String[] result = text.split("\r\n");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < result.length; i++)
+            stringBuilder.append(result[i].substring(number - 1, number)).append("\r\n");
+        return stringBuilder.toString();
     }
 
-    public void cutByDelimeter(int fieldNumber, String separator, String input) {
-        String[] result = input.split("\r\n");
-        for (int i = 0; i < result.length; i++) {
-            String[] content = result[i].split(separator);
-            if (content.length < fieldNumber)
-                System.out.println("\n");
-            else
-                System.out.println(content[fieldNumber - 1]);
-        }
+    public String cutByField(String text) {
+        String[] result = text.split("\r\n");
+        StringBuilder fields = new StringBuilder();
+        for (String aResult : result)
+            fields.append(aResult).append("\r\n");
+        return fields.toString();
     }
+
+    public String cutByFieldAndSeparator(int fieldNumber, String seperator, String text) {
+        String[] result = text.split("\r\n");
+        StringBuilder cut = new StringBuilder();
+        for (String aResult : result) {
+            String[] data = aResult.split(seperator);
+            if (data.length < fieldNumber)
+                cut.append("\r\n");
+            else cut.append(data[fieldNumber - 1]).append("\r\n");
+        }
+        return cut.toString();
+    }
+
 }
