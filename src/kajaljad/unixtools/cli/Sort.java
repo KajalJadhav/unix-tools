a9b3f4db49fb470b6fc98007a11b98daccc034f4
Kajal <kajal@inankhome.com>
2014-01-15 01:03:28 +0530

    Changes Done in Sort

--- src/kajaljad/unixtools/cli/Sort.java
+++ src/kajaljad/unixtools/cli/Sort.java
@@ -6,21 +6,20 @@ import kajaljad.unixtools.libraries.SortOperations;
 public class Sort {
     public static void main(String[] args) {
         String[] sortedData;
-        SortOperations operations = new SortOperations();
-        MyFileReader fileData = new MyFileReader();
+        SortOperations so = new SortOperations();
+        MyFileReader rf = new MyFileReader();
         String fileContent;
         if (args.length == 2 && 0 == args[0].compareTo("-r")) {
-            fileContent = fileData.readFile(args[1]);
-            String[] reverse = operations.reverseData(fileContent);
-            for (String s : reverse) {
+            fileContent = rf.readFile(args[1]);
+            String[] reverse = so.reverseData(fileContent);
+            for (String s : reverse)
                 System.out.println(s);
-            }
         } else {
-            fileContent = fileData.readFile(args[0]);
-            sortedData = operations.sort(fileContent);
-            for (String s : sortedData) {
+            fileContent = rf.readFile(args[0]);
+            sortedData = so.sort(fileContent);
+            for (String s : sortedData)
                 System.out.println(s);
-            }
         }
     }
 }
+
