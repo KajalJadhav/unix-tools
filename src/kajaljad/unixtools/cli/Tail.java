fb058578b5fa24aa46fd91ca31b785e617fddfa5
Kajal <kajal@inankhome.com>
2014-01-15 10:40:57 +0530

    Sequence Handled in Tail

--- src/kajaljad/unixtools/cli/Tail.java
+++ src/kajaljad/unixtools/cli/Tail.java
@@ -5,23 +5,40 @@ import kajaljad.unixtools.libraries.TailOperations;
 
 public class Tail {
     public static void main(String[] args) {
-        MyFileReader rf = new MyFileReader();
         int length = 10;
-        String fileData;
         String fileName = args[0];
-        if (args.length == 2) {
-            length = Math.abs(Integer.parseInt(fileName));
-            fileName = args[1];
+        String fileData;
+        StringBuilder stringBuilder = new StringBuilder();
+        Tail tail = new Tail();
+        MyFileReader readContent = new MyFileReader();
+        String[] processArgs = tail.getArguments(args);
+        if (processArgs[1] != null) {
+            length = Math.abs(Integer.parseInt(processArgs[1]));
+            fileName = processArgs[0];
         }
-        fileData = rf.readFile(fileName);
+        fileData = readContent.readFile(fileName);
         TailOperations tailOperations = new TailOperations(length, fileData);
         String lines = tailOperations.tail();
         String[] result = lines.split("\r\n");
-        StringBuilder stringBuilder = new StringBuilder();
         for (String line : result) {
             stringBuilder.append(line);
             stringBuilder.append("\n");
         }
         System.out.println(stringBuilder.toString());
+    }
+
+    String[] getArguments(String[] args) {
+        String[] options = new String[2];
+        for (int i = 0; i < args.length; i++) {
+            if (Tail.isNumber(args[i]))
+                options[1] = args[i];
+            if (!Tail.isNumber(args[i]))
+                options[0] = args[i];
+        }
+        return options;
+    }
+
+    static boolean isNumber(String args) {
+        return args.matches("-.*");
     }
 }
