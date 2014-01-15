373fb7ce0a5d1ecc712637cda5cd9c54a0fad454
Kajal <kajal@inankhome.com>
2014-01-15 18:29:09 +0530

    Shell Files

--- kajCut.sh
+++ kajCut.sh
@@ -1,1 +1,1 @@
-java -cp $UNIXTOOLS_SH/out/artifacts/unix_tools/unix-tools.jar kajaljad.unixtools.cli.Cut "${@}"
+java -cp $UNIXTOOLS_SH/unix-tools.jar kajaljad.unixtools.cli.Cut "${@}"
--- kajSort.sh
+++ kajSort.sh
@@ -1,1 +1,1 @@
-java -cp $UNIXTOOLS_SH/out/artifacts/unix_tools/unix-tools.jar kajaljad.unixtools.cli.Sort $1 $2
+java -cp $UNIXTOOLS_SH/unix-tools.jar kajaljad.unixtools.cli.Sort $1 $2
