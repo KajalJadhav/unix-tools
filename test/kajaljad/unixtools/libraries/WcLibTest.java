d1fcfb1280dad85cc010d98f4828035598e7500f
Kajal <kajal@inankhome.com>
2014-01-15 23:35:04 +0530

    Test case for count words in single and count words in multiple lines are done

--- test/kajaljad/unixtools/libraries/WcLibTest.java
+++ test/kajaljad/unixtools/libraries/WcLibTest.java
@@ -18,6 +18,22 @@ public class WcLibTest {
         WcLib operation = new WcLib("Kajal\r\nAnant\r\nJadhav\r\n");
         int expected = 3;
         int actual = operation.countLines();
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countWordsInASingleLine() throws Exception {
+        WcLib operation = new WcLib("Kajal Laxit Vaidehi");
+        int expected = 3;
+        int actual = operation.countWords();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countWordsInMultipleLines() throws Exception {
+        WcLib operation = new WcLib("Kajal Anant Jadhav\r\nLaxit Anant Jadhav");
+        int expected = 6;
+        int actual = operation.countWords();
+        assertEquals(expected, actual);
     }
 }
