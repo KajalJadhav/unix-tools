aab9ea3cd3cacd76e36e0e1da3621d9e32693266
Kajal <kajal@inankhome.com>
2014-01-16 02:00:11 +0530

    Test case for get tail lines when number of lines are specifies and when not specified is done

--- test/kajaljad/unixtools/libraries/TailOperationsTest.java
+++ test/kajaljad/unixtools/libraries/TailOperationsTest.java
@@ -20,4 +20,22 @@ public class TailOperationsTest {
         int actual = operations.getNumberOfLines(args);
         assertEquals(5, actual);
     }
+
+    @Test
+    public void getTailLinesWhenNumberOfLinesAreNotSpecified() throws Exception {
+        TailOperations operations = new TailOperations();
+        String input = "Kajal\r\nAnant\r\nJadhav\r\nLaxit\r\nAnant\r\nJadhav\r\nVaidehi\r\nSunil\r\nJadhav\r\nAnant\r\n";
+        String expected = "Kajal\r\nAnant\r\nJadhav\r\nLaxit\r\nAnant\r\nJadhav\r\nVaidehi\r\nSunil\r\nJadhav\r\nAnant\r\n";
+        String actual = operations.getTailLines(input, 10);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getTailLinesWhenNumberOfLinesAreSpecified() throws Exception {
+        TailOperations operations = new TailOperations();
+        String input = "Kajal\r\nAnant\r\nJadhav\r\nLaxit\r\nAnant\r\nJadhav\r\nVaidehi\r\nSunil\r\nJadhav\r\n";
+        String expected = "Anant\r\nJadhav\r\nVaidehi\r\nSunil\r\nJadhav\r\n";
+        String actual = operations.getTailLines(input, 5);
+        assertEquals(expected, actual);
+    }
 }
