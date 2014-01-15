278fd7461272a3ce3f1e354a755ccb54a3a9fd08
Kajal <kajal@inankhome.com>
2014-01-16 01:52:14 +0530

    Tail Refactored

--- src/kajaljad/unixtools/cli/Tail.java
+++ src/kajaljad/unixtools/cli/Tail.java
@@ -5,40 +5,24 @@ import kajaljad.unixtools.libraries.TailOperations;
 
 public class Tail {
     public static void main(String[] args) {
-        int length = 10;
-        String fileName = args[0];
+        String fileName = "";
         String fileData;
-        StringBuilder stringBuilder = new StringBuilder();
-        Tail tail = new Tail();
-        MyFileReader readContent = new MyFileReader();
-        String[] processArgs = tail.getArguments(args);
-        if (processArgs[1] != null) {
-            length = Math.abs(Integer.parseInt(processArgs[1]));
-            fileName = processArgs[0];
+        String result;
+        TailOperations operations = new TailOperations();
+        MyFileReader rf = new MyFileReader();
+        int noOfLines;
+        try {
+            fileName = operations.getFileName(args);
+            noOfLines = operations.getNumberOfLines(args);
+            fileData = rf.readFile(fileName);
+            result = operations.getTailLines(fileData,noOfLines);
+        } catch (NullPointerException ex) {
+            System.err.println("Please Specify File Name");
+            return;
+        } catch (Exception ex) {
+            System.err.println(" " + fileName + " :No such File or Directory");
+            return;
         }
-        fileData = readContent.readFile(fileName);
-        TailOperations tailOperations = new TailOperations(length, fileData);
-        String lines = tailOperations.tail();
-        String[] result = lines.split("\r\n");
-        for (String line : result) {
-            stringBuilder.append(line);
-            stringBuilder.append("\n");
-        }
-        System.out.println(stringBuilder.toString());
-    }
-
-    String[] getArguments(String[] args) {
-        String[] options = new String[2];
-        for (int i = 0; i < args.length; i++) {
-            if (Tail.isNumber(args[i]))
-                options[1] = args[i];
-            if (!Tail.isNumber(args[i]))
-                options[0] = args[i];
-        }
-        return options;
-    }
-
-    static boolean isNumber(String args) {
-        return args.matches("-.*");
+        System.out.println(result);
     }
 }
--- src/kajaljad/unixtools/libraries/TailOperations.java
+++ src/kajaljad/unixtools/libraries/TailOperations.java
@@ -1,20 +1,31 @@
 package kajaljad.unixtools.libraries;
 
 public class TailOperations {
-    private String text;
-    private int limit;
 
-    public TailOperations(int noOfLines, String text) {
-        this.text = text;
-        this.limit = noOfLines;
-    }
-
-    public String tail() {
-        String[] lines = text.split("\r\n");
-        if (lines.length < limit) return lines.toString();
+    public String getTailLines(String fileData, int noOfLines) {
+        String[] lines = fileData.split("\r\n");
         StringBuilder result = new StringBuilder();
-        for (int i = (lines.length - limit); i < lines.length; i++)
+        for (int i = (lines.length - noOfLines); i < lines.length; i++)
             result.append(lines[i]).append("\r\n");
         return result.toString();
+    }
+
+    public int getNumberOfLines(String[] args) {
+        int defaultNumberOfLines = 10;
+        for (String arg : args) {
+            if (arg.startsWith("-n")) {
+                return Integer.parseInt(arg.replace("-n", ""));
+            }
+        }
+        return defaultNumberOfLines;
+    }
+
+    public String getFileName(String[] args) {
+        for (String arg : args) {
+            if (!arg.startsWith("-")) {
+                return arg;
+            }
+        }
+        return null;
     }
 }
