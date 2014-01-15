38e8fca12a4d09a959c9579118f24ae0cb20c20b
Kajal <kajal@inankhome.com>
2014-01-16 01:55:19 +0530

    Test case for get number of lines when specified and when not specified is done in tail

--- test/kajaljad/unixtools/libraries/TailOperationsTest.java
+++ test/kajaljad/unixtools/libraries/TailOperationsTest.java
@@ -5,21 +5,19 @@ import org.junit.Test;
 import static junit.framework.Assert.assertEquals;
 
 public class TailOperationsTest {
-    String input = "Kajal\r\nAnant\r\nJadhav\r\nLaxit\r\nAnant\r\nJadhav\r\nVaidehi\r\nSunil\r\nJadhav\r\nAnant\r\nShankar\r\nJadhav\r\n";
-
     @Test
-    public void test_display_tail_for_specific_lines() throws Exception {
-        TailOperations tailOperations = new TailOperations(3, input);
-        String expected = "Anant\r\nShankar\r\nJadhav\r\n";
-        String actual = tailOperations.tail();
-        assertEquals(expected, actual);
+    public void getNumberOfLinesWhenNumberOfLinesAreNotSpecified() throws Exception {
+        TailOperations operations = new TailOperations();
+        String[] args = {"names.txt"};
+        int actual = operations.getNumberOfLines(args);
+        assertEquals(10, actual);
     }
 
     @Test
-    public void test_display_default_tail() throws Exception {
-        TailOperations tailOperations = new TailOperations(10, input);
-        String expected = "Jadhav\r\nLaxit\r\nAnant\r\nJadhav\r\nVaidehi\r\nSunil\r\nJadhav\r\nAnant\r\nShankar\r\nJadhav\r\n";
-        String actual = tailOperations.tail();
-        assertEquals(expected, actual);
+    public void getNumberOfLinesWhenNumberOfLinesAreSpecified() throws Exception {
+        TailOperations operations = new TailOperations();
+        String[] args = {"-n5", "names.txt"};
+        int actual = operations.getNumberOfLines(args);
+        assertEquals(5, actual);
     }
 }
