1c28c50e2bb325869b4cd8fae91a092bee2564c1
Kajal <kajal@inankhome.com>
2014-01-16 01:06:19 +0530

    Test case for when number of lines are specified and when not specified is done

--- test/kajaljad/unixtools/libraries/HeadLibTest.java
+++ test/kajaljad/unixtools/libraries/HeadLibTest.java
@@ -5,21 +5,20 @@ import org.junit.Test;
 import static junit.framework.Assert.assertEquals;
 
 public class HeadLibTest {
-    String input = "Kajal\r\nAnant\r\nJadhav\r\nLaxit\r\nAnant\r\nJadhav\r\nVaidehi\r\nSunil\r\nJadhav\r\nAnant\r\nShankar\r\nJadhav\r\n";
-
     @Test
-    public void testHead() throws Exception {
-        HeadLib headLib = new HeadLib(3, input);
-        String expected = "Kajal\r\nAnant\r\nJadhav\r\n";
-        String actual = headLib.head();
-        assertEquals(expected, actual);
+    public void getNumberOfLinesWhenNumberOfLinesAreNotSpecified() throws Exception {
+        HeadLib operations = new HeadLib();
+        String[] args = {"names.txt"};
+        int actual = operations.getNumberOfLines(args);
+        assertEquals(10, actual);
     }
 
     @Test
-    public void test_display_default_head() throws Exception {
-        HeadLib headLib = new HeadLib(10, input);
-        String expected = "Kajal\r\nAnant\r\nJadhav\r\nLaxit\r\nAnant\r\nJadhav\r\nVaidehi\r\nSunil\r\nJadhav\r\nAnant\r\n";
-        String actual = headLib.head();
-        assertEquals(expected, actual);
+    public void getNumberOfLinesWhenNumberOfLinesAreSpecified() throws Exception {
+        HeadLib operations = new HeadLib();
+        String[] args = {"-n5", "names.txt"};
+        int actual = operations.getNumberOfLines(args);
+        assertEquals(actual, 5);
     }
 }
+
