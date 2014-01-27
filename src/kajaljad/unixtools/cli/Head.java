b009ebb8870b35c9908622d59ee2d33f2824a53e
Kajal <kajal@inankhome.com>
2014-01-27 10:01:52 +0530

    Config file done in head and tail

--- src/kajaljad/unixtools/cli/Head.java
+++ src/kajaljad/unixtools/cli/Head.java
@@ -3,25 +3,36 @@ package kajaljad.unixtools.cli;
 import kajaljad.unixtools.libraries.HeadLib;
 import kajaljad.unixtools.filesystem.MyFileReader;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Head {
     public static void main(String[] args) {
-        String fileName = "";
-        String fileData, result;
+        String fileName = null;
+        InputStream is = null;
+        String fileData = null;
+        String result = null;
         HeadLib operations = new HeadLib();
         MyFileReader rf = new MyFileReader();
+        Properties prop = new Properties();
         int noOfLines;
         try {
             fileName = operations.getFileName(args);
-            noOfLines = operations.getNumberOfLines(args);
             fileData = rf.readFile(fileName);
-            result = operations.getHeadLines(fileData, noOfLines);
+            noOfLines = operations.getNumberOfLines(args);
+            if(args.length < 2){
+                is = new FileInputStream(System.getenv("UNIXTOOLS_SH")+"/lib/config.properties");
+                prop.load(is);
+                noOfLines = Integer.parseInt(prop.getProperty("default.head.properties"));
+            }
         } catch (NullPointerException ex) {
             System.err.println("Please Specify File Name");
             return;
         } catch (Exception ex) {
-            System.err.println(" " + fileName + " :No such File or Directory");
-            return;
+            noOfLines = 10;
         }
+        result = operations.getHeadLines(fileData, noOfLines);
         System.out.println(result);
     }
 }
--- src/kajaljad/unixtools/cli/Tail.java
+++ src/kajaljad/unixtools/cli/Tail.java
@@ -3,26 +3,36 @@ package kajaljad.unixtools.cli;
 import kajaljad.unixtools.filesystem.MyFileReader;
 import kajaljad.unixtools.libraries.TailOperations;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Tail {
     public static void main(String[] args) {
-        String fileName = "";
-        String fileData;
+        String fileName = null;
+        InputStream is = null;
+        String fileData = null;
         String result;
         TailOperations operations = new TailOperations();
         MyFileReader rf = new MyFileReader();
+        Properties prop = new Properties();
         int noOfLines;
         try {
             fileName = operations.getFileName(args);
-            noOfLines = operations.getNumberOfLines(args);
             fileData = rf.readFile(fileName);
-            result = operations.getTailLines(fileData,noOfLines);
+            noOfLines = operations.getNumberOfLines(args);
+            if (args.length < 2) {
+                is = new FileInputStream(System.getenv("UNIXTOOLS_SH") + "/lib/myconfig.properties");
+                prop.load(is);
+                noOfLines = Integer.parseInt(prop.getProperty("default.tail.properties"));
+            }
         } catch (NullPointerException ex) {
             System.err.println("Please Specify File Name");
             return;
         } catch (Exception ex) {
-            System.err.println(" " + fileName + " :No such File or Directory");
-            return;
+            noOfLines = 10;
         }
+        result = operations.getTailLines(fileData, noOfLines);
         System.out.println(result);
     }
 }
